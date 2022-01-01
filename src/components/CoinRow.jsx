a146e6132f4e5323aba8f9becbe4a305e4014b49
Olivo <46498127+MrOlivo@users.noreply.github.com>
2022-01-01 11:08:24 -0700

    Update CoinRow.jsx

--- src/components/CoinRow.jsx
+++ src/components/CoinRow.jsx
@@ -1,7 +1,8 @@
-import React from "react";
+import styles from "./CoinRow.module.css";
 
 export default function CoinRow({ coin, index }) {
   let color = coin.price_change_24h > 0 ? "text-success" : "text-danger";
+
   return (
     <tr>
       <td>{index}</td>
@@ -9,11 +10,12 @@ export default function CoinRow({ coin, index }) {
         <img
           src={coin.image}
           alt={coin.name + " logo."}
-          className="me-4 img"
-          style={{ width: "1rem" }}
+          className={"me-4 " + styles.img}
         />
         <span>{coin.name}</span>
-        <span className="ms-2 text-muted text-uppercase">{coin.symbol}</span>
+        <span className={"ms-2 text-uppercase " + styles["text-iso"]}>
+          {coin.symbol}
+        </span>
       </td>
       <td className="text-end">{coin.current_price}</td>
       <td className={color + " text-end"}>{coin.price_change_24h}</td>
