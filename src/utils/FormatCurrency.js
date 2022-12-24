ea446a721cf8c0551db834663ec75f28d49c51cf
Edwin Olivo Garcia <17003685@uan.edu.mx>
2022-12-24 09:35:50 -0700

    Created Vite project

--- src/utils/FormatCurrency.js
+++
@@ -1,5 +0,0 @@
-export function formatCurrency(amount, currency = "USD") {
-  const options = { style: "currency", currency: currency };
-  const numberFormat = new Intl.NumberFormat("en-US", options);
-  return numberFormat.format(amount);
-}
